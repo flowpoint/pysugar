e6f2b7cff24c3e956f44a5e1c8255eb1b54f7672
flowpoint <flowpoint@protonmail.com>
2025-05-28 21:06:59 +0200

    update

---
+++ README.md
@@ -0,0 +1,6 @@
+syntactic sugar for the python language
+
+`pip install git+https://github.com/flowpoint/pysugar.git`
+
+`from pysugar import *`
+
--- src/pysugar/__init__.py
+++ src/pysugar/__init__.py
@@ -1,9 +1,14 @@
 from functools import reduce, partial
 from math import ceil, floor
 from statistics import mean
+from time import sleep
+import sys.stderr 
 
 def efilter(*args, **kwargs):
     return list(filter(*args, **kwargs))
 
 def emap(*args, **kwargs):
     return list(map(*args, **kwargs))
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
