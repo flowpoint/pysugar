8990d85b28285956f7edc36c7a8fb23cc66bbec1
flowpoint <flowpoint@protonmail.com>
2026-02-24 15:41:58 +0100

    add single, and pathlib Path to default imports

--- src/pysugar/__init__.py
+++ src/pysugar/__init__.py
@@ -1,12 +1,13 @@
 from functools import reduce, partial
 from itertools import accumulate
-from more_itertools import flatten, chunked
+from more_itertools import flatten, chunked, unique
 from math import ceil, floor
 from statistics import mean
 from time import sleep
 import sys
 
 from copy import copy
+from pathlib import Path
 
 def efilter(*args, **kwargs):
     ''' eager filter '''
@@ -25,3 +26,13 @@ def dpop(k, d):
     d2 = copy(d)
     d2.pop(k, None)
     return d2
+
+class BadGeneratorError(ValueError):
+    pass
+
+def single(x: list):
+    ''' get the element from a single element generator '''
+    xl = list(x)
+    if len(xl) != 1:
+        raise BadGeneratorError('generator should produce only a single element')
+    return x[0]
